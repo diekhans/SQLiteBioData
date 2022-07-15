7434f17c47648387b1dc536c6f61f9188d2efaae
Mark Diekhans <markd@ucsc.edu>
2022-07-14 20:28:01 -0700

    moved code to generate NCBI gene database from another project to here

---
+++ defs.mk
@@ -0,0 +1,27 @@
+# include with
+#   ROOT=../..
+#   include ${ROOT}/defs.mk
+
+.SECONDARY:
+
+MAKEFLAGS = --no-builtin-rules
+SHELL = /bin/bash -e
+
+binDir = ${ROOT}/bin
+libDir = ${ROOT}/lib
+
+PYTHON = python3
+export PYTHONPATH = ${libDir}
+
+FLAKE8 = python3 -m flake8
+PYTEST_FLAGS = -s -vv --tb=native
+PYTEST = ${PYTHON} -m pytest ${PYTEST_FLAGS}
+
+export PYTHONWARNINGS=always
+
+diff = diff -u
+
+tmpExt = $(shell hostname).$(shell echo $$PPID).tmp
+tmpExtGz = ${tmpExt}.gz
+
+refseqGeneDbBuild = ${binDir}/refseqGeneDbBuild
